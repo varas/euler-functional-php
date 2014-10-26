4d44e17823e2ee3d6613fcccbae927189f09e357
jhvaras <jhvaras@gmail.com>
2014-10-26 17:15:50 +0100

    app

---
+++ src/app.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$problemNumbers = [1, 2];
+
+$problemFactory = function($problemNumber) {
+    $problemClass = 'Varas\Euler\Problem'.$problemNumber;
+    return new $problemClass();
+};
+
+$problemPrinter = function($problem) {
+    echo $problem->getTitle().": ".$problem->getSolution()."\n";
+};
+
+$problems = array_map($problemFactory, $problemNumbers);
+
+array_map($problemPrinter, $problems);
